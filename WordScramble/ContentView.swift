f2ba6399f0748d1a2fbb61c4c02dbb3c5cac7c07
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-29 22:19:13 +0100

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,177 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Danjuma Nasiru on 07/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    @State private var score = 0
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+            
+            List {
+                
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
+                    }
+                }.padding()
+                
+            }.listStyle(.plain)
+            
+                .navigationTitle(rootWord)
+                .onSubmit {
+                    addNewWord()
+                }
+                .toolbar(content: {
+                    Button("Start Over", action: {startGame()})
+                })
+                .onAppear(perform: startGame)
+                .alert(errorTitle, isPresented: $showingError) {
+                    Button("OK", role: .cancel) { }
+                } message: {
+                    Text(errorMessage)
+                }
+                Spacer()
+                Text("Your Score is: \(score)").font(.title).frame( alignment: .leading)
+                Spacer()
+                Spacer()
+                Spacer()
+            }
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+        
+        guard sameAsRootWord(word: answer) else{
+            newWord = ""
+            wordError(title: "Can't have word same as root word", message: "Try forming a different word from root word")
+            return
+        }
+        guard tooShort(word: answer) else{
+            newWord = ""
+            wordError(title: "Word too short", message: "Try a word with 3 or more characters")
+            return
+        }
+        
+        guard isOriginal(word: answer) else{
+            newWord = ""
+            wordError(title: "Word already used", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            newWord = ""
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            newWord = ""
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation{
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+        score += 2
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords.removeAll()
+                score = 0
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+        
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func sameAsRootWord(word: String) -> Bool{
+        word != rootWord
+    }
+    
+    func tooShort(word: String) -> Bool{
+        word.count > 2
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        //if our word is spelled correctly, it returns nsnotfound else it returns the range where the mispelled word was found.
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Danjuma Nasiru on 07/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
